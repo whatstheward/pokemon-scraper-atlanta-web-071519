13a57234886a3791cf50081c1d7557f963081bb4
Maddie Ward <mward@NYC-US16507-MBP.local>
2019-07-23 15:16:10 -0400

    done

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,2 +1,5 @@
 -- Write your sql commands to create your db schema here.
+
+CREATE TABLE IF NOT EXISTS pokemon (id INTEGER PRIMARY KEY, name TEXT, type TEXT);
+
 -- It should create a table in a new table in the db/ directory.
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
-end
+    attr_accessor :id, :name, :type, :hp, :db
+    
+    def initialize(id:, name:, type:, hp: nil, db:)
+    @id = id 
+    @name = name 
+    @type = type
+    @hp = hp
+    @db = db    
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES(?, ?)", name, type)
+    end
+
+    def self.find(id, db)
+        pokemon = db.execute("SELECT * FROM pokemon WHERE id=#{id}")
+        Pokemon.new(id: pokemon[0][0], name: pokemon[0][1], type: pokemon[0][2], hp: pokemon[0][3], db: db)
+    end
+
+    def alter_hp(new_hp, db)
+        db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+    end
+
+  end
